1eab6cf682a863ae771d8fb4520087ec73100f28
Saul <saul.crespo.sc@gmail.com>
2023-04-19 12:07:06 +0200

    Este es el primer Commit del repositorio.

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author saulc
+ *
+ */
+module rep_03 {
+}
---
+++ src/rep_03/Main.java
@@ -0,0 +1,7 @@
+package rep_03;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("¡Hola cariño! Soy Saúl y te amo un montonazo.");
+	}
+}
